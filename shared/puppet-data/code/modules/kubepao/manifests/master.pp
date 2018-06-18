60d3d4d00ffffeb480279e07b39a9847fe0cd808
Paolo Vitali <paolo.vitali>
2018-06-18 06:58:02 +0200

    Fixed kubepao master manifest

--- shared/puppet-data/code/modules/kubepao/manifests/master.pp
+++ shared/puppet-data/code/modules/kubepao/manifests/master.pp
@@ -1,19 +1,19 @@
 class kubepao::master inherits kubepao {
 
-exec { "kubeadm_init":
-  command => "kubeadm init",
-  path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
-  onlyif => "[ !  -f /etc/kubernetes/admin.conf ]"
-}
-
-if $kube_master_cni_provider == "flannel"  {
-
-  exec { "kubectl_apply_net":
-  command => "kubectl apply -f $kube_master_cni_url",
-  path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
-  onlyif => "[ $(kubectl get pods -n kube-system --field-selector=status.phase=Running |grep kube-dns) != " ]"
+  # Master Init
+  exec { "kubeadm_init":
+    command => "kubeadm init --pod-network-cidr=$kube_master_pod_cidr",
+    path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
+    onlyif => "[ !  -f /etc/kubernetes/admin.conf ]"
   }
 
-}
+ # Cni Setup
+  if $kube_master_cni_provider == "flannel"  {
+    exec { "kubectl_apply_net":
+      command => "kubectl apply -f $kube_master_cni_url",
+      path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
+      onlyif => "[ -z $(kubectl get pods -n kube-system --field-selector=status.phase=Running | grep kube-dns) ]"
+    }
+  } 
 
 }
