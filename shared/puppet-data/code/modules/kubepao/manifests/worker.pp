50166248723ec78bc44ea173f265d9cb411f34e8
Paolo Vitali <paolo.vitali>
2018-06-19 08:08:00 +0200

    added worker join first test

--- shared/puppet-data/code/modules/kubepao/manifests/worker.pp
+++ shared/puppet-data/code/modules/kubepao/manifests/worker.pp
@@ -1,3 +1,11 @@
-class kube-pao::worker inherits kube-pao {
+class kubepao::worker inherits kubepao {
+
+  # Worker Init
+  exec { "kubeadm_join":
+    command => "kubeadm join --token $kube_master_token --token-ttl $kube_master_token_ttl --discovery-token-unsafe-skip-ca-verification $kube_master_ext_ip:6443",
+    path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
+    onlyif => "[ !  -f /etc/kubernetes/admin.conf ]"
+  }
+
 
 }
