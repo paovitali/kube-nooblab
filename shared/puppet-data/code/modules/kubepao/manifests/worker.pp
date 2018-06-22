ced5d52767c463fcea067d5807071981e9de02d9
Paolo Vitali <paolo.vitali>
2018-06-22 18:32:08 +0200

    Reviewed worker join command

--- shared/puppet-data/code/modules/kubepao/manifests/worker.pp
+++ shared/puppet-data/code/modules/kubepao/manifests/worker.pp
@@ -2,10 +2,9 @@ class kubepao::worker inherits kubepao {
 
   # Worker Init
   exec { "kubeadm_join":
-    command => "kubeadm join --token $kube_master_token --token-ttl $kube_master_token_ttl --discovery-token-unsafe-skip-ca-verification $kube_master_ext_ip:6443",
+    command => "kubeadm join --token $kube_worker_join_token  --discovery-token-ca-cert-hash sha256:$kube_worker_join_certhash $kube_worker_join_masterip:6443",
     path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
     onlyif => "[ !  -f /etc/kubernetes/admin.conf ]"
   }
-
 
 }
