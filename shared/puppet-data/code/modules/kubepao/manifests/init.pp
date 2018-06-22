8c4a332624840579e35604143455446a9c478b33
Paolo Vitali <paolo.vitali>
2018-06-22 18:31:01 +0200

    Added general and master/worker params

--- shared/puppet-data/code/modules/kubepao/manifests/init.pp
+++ shared/puppet-data/code/modules/kubepao/manifests/init.pp
@@ -10,10 +10,13 @@ $kube_master_cni_url="",
 $kube_master_token="",
 $kube_master_token_ttl="",
 $kube_master_ext_ip="",
-$kube_master_token_cmd_file="",
+$kube_master_join_certhash_file="",
 
 # Kubernetes Worker Parameters
 $kube_worker_role="false",
+$kube_worker_join_token="",
+$kube_worker_join_certhash="",
+$kube_worker_join_masterip="",
 
 ) {
 
@@ -25,6 +28,7 @@ include kubepao::common
 
 # KUBERNETES MASTER ROLE CLASSES
 if $kube_master_role == "true" {
+#   include kubepao::common
    include kubepao::master
 }
 
