0b52df480a2ede5cfedf9918df5552e6239057a1
Paolo Vitali <paolo.vitali>
2018-06-22 18:31:46 +0200

    Reviewed dependencies and conditions on k8s master init

--- shared/puppet-data/code/modules/kubepao/manifests/master.pp
+++ shared/puppet-data/code/modules/kubepao/manifests/master.pp
@@ -2,25 +2,29 @@ class kubepao::master inherits kubepao {
 
   # Master Init
   exec { "kubeadm_init":
-    command => "kubeadm init --pod-network-cidr=$kube_master_pod_cidr --token=$kube_master_token --token-ttl=$kube_master_token_ttl ",
+    command => "kubeadm init --pod-network-cidr=$kube_master_pod_cidr --apiserver-advertise-address=$ipaddress_eth1 --token-ttl=0",
     path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
     onlyif => "[ !  -f /etc/kubernetes/admin.conf ]"
   }
 
- # Cni Setup
+  # Cni Setup
   if $kube_master_cni_provider == "flannel"  {
     exec { "kubectl_apply_net":
+      environment => [ "KUBECONFIG=/etc/kubernetes/admin.conf" ],
       command => "kubectl apply -f $kube_master_cni_url",
       path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
-      onlyif => "[ ! -f /opt/cni/bin/flannel  ]"
+      tries => 10,
+      try_sleep => 5,
+      onlyif => "[ ! -f  /run/flannel/subnet.env ] || [ $(kubectl get nodes|grep master|awk '{print \$2}') == 'NotReady' ]",
+      require => Service[ "kubelet" ]  
     }
   } 
 
- # Cluster Tokens creation
+  # Cluster Tokens creation
   exec { "kubeadm_token_create":
-    command => "kubeadm token create --ttl 0 --print-join-command > $kube_master_token_cmd_file",
+    command => "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > $kube_master_join_certhash_file",
     path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
-    onlyif => "[ ! -f $kube_master_token_cmd_file ]"
+    onlyif => "[ ! -f $kube_master_join_cmd_file ]"
   }
 
-} 
+}
